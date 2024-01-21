02fe9a21de56bebf5f801753491641d881a61e97
Nihit Reddy <nihitavr@gmail.com>
2024-01-21 18:42:05 +0530

    Added animated animation

--- src/components/ui/profile-loading-skeleton.tsx
+++ src/components/ui/profile-loading-skeleton.tsx
@@ -1,14 +1,21 @@
+import Image from "next/image";
 import { Skeleton } from "./skeleton";
 
 export default function ProfileLoadingSkeleton() {
   return (
-    <div className="flex flex-col gap-8">
-      <Skeleton className="h-80 w-full rounded-lg bg-slate-300" />
+    <div className="flex h-[90vh] w-full flex-col items-center justify-center">
+      <Image
+        src="/loading-cute-animated-cat.gif"
+        alt="Animated Cat"
+        width={300}
+        height={300}
+      />
+      {/* <Skeleton className="h-80 w-full rounded-lg bg-slate-300" />
       <div className="flex flex-col gap-3">
         <Skeleton className="h-7 w-32 flex-shrink rounded-md" />
         <Skeleton className="h-16 w-full flex-shrink rounded-md" />
         <Skeleton className="h-32 w-full rounded-md" />
-      </div>
+      </div> */}
     </div>
   );
 }
