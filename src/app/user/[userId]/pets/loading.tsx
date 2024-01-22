fc068f9a0185782d0aee40a916f39e9946d60aa7
Nihit Reddy <nihitavr@gmail.com>
2024-01-23 04:31:35 +0530

    Changed Share Button implementation

---
+++ src/app/user/[userId]/pets/loading.tsx
@@ -0,0 +1,17 @@
+import { Skeleton } from "~/components/ui/skeleton";
+
+export default function PetsListLoadingSkeleton() {
+  return (
+    <div className="mt-5 flex w-full flex-col items-center">
+      <Skeleton className="h-6 w-52 rounded-lg bg-slate-200" />
+      <div className="w-full p-3 lg:w-2/5">
+        <div className="grid grid-cols-2 gap-3 ">
+          <Skeleton className="h-72 rounded-lg bg-slate-200" />
+          <Skeleton className="h-72 rounded-lg bg-slate-200" />
+          <Skeleton className="h-72 rounded-lg bg-slate-200" />
+          <Skeleton className="h-72 rounded-lg bg-slate-200" />
+        </div>
+      </div>
+    </div>
+  );
+}
